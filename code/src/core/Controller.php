358fd9a6ff65ed5c2c414def1ad8a4fc97e05401
sasor <s.leamsi99@gmail.com>
2018-04-14 14:45:29 -0400

    [FIX] actualizando la ruta de models y views

--- code/src/core/Controller.php
+++ code/src/core/Controller.php
@@ -4,12 +4,12 @@ class Controller
 {
     public function model($model)
     {
-        require_once '../models/'.$model.'.php';
+        require_once '../src/models/'.$model.'.php';
         return new $model();
     }
     
     public function view($view, $data = [])
     {
-        require_once '../views/'.$view.'.php';
+        require_once '../src/views/'.$view.'.php';
     }
 }
