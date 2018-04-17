d2c9a03d22bb13ff62810a949c2480dc524531fe
owenwilson <owenwilson76467963@gmail.com>
2018-04-17 18:42:36 -0400

    fix index

--- code/src/controllers/home.php
+++ code/src/controllers/home.php
@@ -4,13 +4,8 @@ class Home extends Controller
 {
     public function index()
     {
-        echo 'Home index';
-        //$a = DB::instance();
-        //print_r($a->getConnection());
-        $user = $this->model('UserModel');
-        print_r($user->linker());
-        //$user->name = 'Joe';
-        //$this->view('home/index', ['name' => $user->name]);
+        //$this->view('home/index');
+        echo "adsagsdgfasdfa";
     }
     public function contact()
     {
