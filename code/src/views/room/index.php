74696198a5734b4bff261ccb7dafcf4faded9bec
alex <alexander.unico.13@gmail.com>
2018-04-17 19:43:31 -0400

    datos fake DB

--- code/src/views/room/index.php
+++ code/src/views/room/index.php
@@ -1,6 +1,5 @@
 <?php
    foreach ($data['rooms'] as $room) {
     echo'<pre>'; 
-    print_r($room);
-    
-   }
+    echo($room->start);
+}
---
+++ database/room.sql
@@ -0,0 +1,5 @@
+INSERT INTO `room` (`id`, `name_room`, `max_capacity_chairs`, `current_chairs`, `defective_chairs`) VALUES
+(1, '690A', 45, 40, 10),
+(2, '690B', 50, 40, 5),
+(3, '690C', 60, 50, 4),
+(4, '690D', 65, 45, 15);
