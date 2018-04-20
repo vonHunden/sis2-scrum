7304d247dcac64853eb9a0294f463ca6447e775f
rodrigo <salguero_ro@hotmail.com>
2018-04-20 00:55:22 -0300

    Revert "fixed up index php nav-link ROOM"
    This reverts commit 60feb4b01c3e932b8dba23ef0dc2d209ffd1c130.

--- code/src/views/home/index.php
+++ code/src/views/home/index.php
@@ -20,9 +20,6 @@
       <li class="nav-item active">
         <a class="nav-link" href=<?php echo _HOME."";?>>Inicio <span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href=<?php echo _HOME."/room";?>> Aulas</a>
-      </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Categoria
